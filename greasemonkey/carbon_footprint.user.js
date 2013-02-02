4d76588713dc0982daf22cd42edaa238a43eed2c
Gustavo Recio <gustavo.recio@spindriftgroup.com>
2013-02-02 12:40:25 +0000

    Adding icon

--- greasemonkey/carbon_footprint.user.js
+++ greasemonkey/carbon_footprint.user.js
@@ -6,15 +6,36 @@
 // @grant       none
 // ==/UserScript==
 
+function addCarbonCreditImage(node) {
+	node.text("");
+	node.prepend('<img id="carbon-credit-image" src="http://www.thecarbonfarmer.ca/images/home/home_icon_carbon_credits.png" width="60%" height="60%"/>');
+
+}
+
 function processResponse(data) {
-	alert("Woaaa, got something");
+
+	var children = $('div.actPanel').children();
+
+	var spcr = children[0].cloneNode();
+	var content = children[1].cloneNode();
+
+	$('div.actPanel div.u-cb:eq(2)').after(content);
+	$('div.actPanel div.u-cb:eq(3)').after(spcr);
+
+	addCarbonCreditImage($('div.binLable:eq(1)'));
+	
+	unsafeWindow.console.log($('div.binLable:eq(1)').text());
+
+	alert(elements[0]);
 }
 
 function getOffsetCarbonFootprint(position) {
 	unsafeWindow.console.log("hey!!!")
 	var data = {id: itemId, lat: position.coords.latitude, lon: position.coords.longitude};
 	unsafeWindow.console.log("calling backend with data: " + data);
-	$.get("http://ebayhackcarboncred.appspot.com/calc", data, processResponse);
+	//$.get("http://ebayhackcarboncred.appspot.com/calc", data, processResponse);
+
+	processResponse(data);
 	//alert("Item id: " + itemId + " latitud: " + position.coords.latitude + " longitud: " + position.coords.longitude);
 }
 
