b1f1ab8ae59c1b084fa9c9bda6ce18ca2d305d69
Gustavo Recio <gustavo.recio@spindriftgroup.com>
2013-02-02 14:51:22 +0000

    Hack to show green button

--- greasemonkey/carbon_footprint.user.js
+++ greasemonkey/carbon_footprint.user.js
@@ -9,38 +9,41 @@
 function addCarbonCreditImage(node) {
 	node.text("");
 	node.prepend('<img id="carbon-credit-image" src="http://www.thecarbonfarmer.ca/images/home/home_icon_carbon_credits.png" width="60%" height="60%"/>');
-
 }
 
 function processResponse(data) {
 
-	var children = $('div.actPanel').children();
-
-	var spcr = children[0].cloneNode();
-	var content = children[1].cloneNode();
-
-	$('div.actPanel div.u-cb:eq(2)').after(content);
-	$('div.actPanel div.u-cb:eq(3)').after(spcr);
-
-	addCarbonCreditImage($('div.binLable:eq(1)'));
-	
-	unsafeWindow.console.log($('div.binLable:eq(1)').text());
-
-	alert(elements[0]);
+	$('div.actPanel div.u-cb:eq(2)').after('<div class="u-cb">' +
+		'<div id="prcIsum-lbl" class="u-flL lable binLable"><img id="carbon-credit-image" src="http://www.thecarbonfarmer.ca/images/home/home_icon_carbon_credits.png" width="60%" height="60%"/></div>' +
+		'<div id="vi-mskumap-none" class="u-flL w29 vi-price" itemtype="http://schema.org/Offer" itemscope="itemscope" itemprop="offers" style="">' +
+		'<span id="prcIsum" style="" itemprop="price"> £' + data + '</span>' +
+		'<span content="http://schema.org/OnlineOnly" itemprop="availability"></span>' +
+		'<span content="GBP" itemprop="priceCurrency"></span>' +
+		'</div>' +
+		'<div class="u-flL">' +
+		'<a id="binBtn_btn" class="vib bl " vib="vib" href="http://offer.ebay.co.uk/ws/eBayISAPI.dll?BinController&rev=17&item=360580868204&fromPage=2047675&fb=1&gch=1" title="" style=" background: -moz-linear-gradient(center top , #A9F5A9, #04B404) repeat scroll 0 0 transparent;"> Offset CO2</a>' +
+		'</div>');
+	$('div.actPanel div.u-cb:eq(3)').after('<div class="u-cb spcr"></div>');
 }
 
 function getOffsetCarbonFootprint(position) {
-	unsafeWindow.console.log("hey!!!")
-	var data = {id: itemId, lat: position.coords.latitude, lon: position.coords.longitude};
-	unsafeWindow.console.log("calling backend with data: " + data);
-	//$.get("http://ebayhackcarboncred.appspot.com/calc", data, processResponse);
+	var params = {id: itemId, lat: position.coords.latitude, lon: position.coords.longitude};
+	unsafeWindow.console.log("calling backend with data: " + params);
 
-	processResponse(data);
+	$.get("http://ebayhackcarboncred.appspot.com/calc", params, processResponse);
+	//processResponse('0.15');
+
 	//alert("Item id: " + itemId + " latitud: " + position.coords.latitude + " longitud: " + position.coords.longitude);
 }
 
+function findItemId(url) {
+	var lastPart = url.substring(url.lastIndexOf('/') + 1);
+	if (lastPart.indexOf('?') != -1) {
+		lastPart = lastPart.substring(0, lastPart.indexOf('?'));
+	}
 
-var url = document.URL;
-var itemId = url.substring(url.lastIndexOf('/') + 1);
+	return lastPart;
+}
 
+var itemId = findItemId(document.URL);
 navigator.geolocation.getCurrentPosition(getOffsetCarbonFootprint);
