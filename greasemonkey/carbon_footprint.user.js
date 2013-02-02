2be2d60ebd71676383298e82f77b5cc55200311c
Gustavo Recio <gustavo.recio@spindriftgroup.com>
2013-02-02 16:05:33 +0000

    Finished Greasemonkey script

--- greasemonkey/carbon_footprint.user.js
+++ greasemonkey/carbon_footprint.user.js
@@ -6,34 +6,35 @@
 // @grant       none
 // ==/UserScript==
 
-function addCarbonCreditImage(node) {
-	node.text("");
-	node.prepend('<img id="carbon-credit-image" src="http://www.thecarbonfarmer.ca/images/home/home_icon_carbon_credits.png" width="60%" height="60%"/>');
-}
-
 function processResponse(data) {
+	var amount = Math.round(data*100)/100;
 
 	$('div.actPanel div.u-cb:eq(2)').after('<div class="u-cb">' +
-		'<div id="prcIsum-lbl" class="u-flL lable binLable"><img id="carbon-credit-image" src="http://www.thecarbonfarmer.ca/images/home/home_icon_carbon_credits.png" width="60%" height="60%"/></div>' +
+		'<div id="prcIsum-lbl" class="u-flL lable binLable"><img id="carbon-credit-image" src="http://www.thecarbonfarmer.ca/images/home/home_icon_carbon_credits.png" width="60%" height="60%" style="left: 7px;position: relative;top: -12px;"/></div>' +
 		'<div id="vi-mskumap-none" class="u-flL w29 vi-price" itemtype="http://schema.org/Offer" itemscope="itemscope" itemprop="offers" style="">' +
-		'<span id="prcIsum" style="" itemprop="price"> £' + data + '</span>' +
+		'<span id="prcIsumOffset" style="position:relative;top:2px;" itemprop="price"> £' + amount + '</span>' +
 		'<span content="http://schema.org/OnlineOnly" itemprop="availability"></span>' +
 		'<span content="GBP" itemprop="priceCurrency"></span>' +
 		'</div>' +
 		'<div class="u-flL">' +
-		'<a id="binBtn_btn" class="vib bl " vib="vib" href="http://offer.ebay.co.uk/ws/eBayISAPI.dll?BinController&rev=17&item=360580868204&fromPage=2047675&fb=1&gch=1" title="" style=" background: -moz-linear-gradient(center top , #A9F5A9, #04B404) repeat scroll 0 0 transparent;"> Offset CO2</a>' +
-		'</div>');
-	$('div.actPanel div.u-cb:eq(3)').after('<div class="u-cb spcr"></div>');
+		'<form method="post" target="_blank" action="https://www.paypal.com/cgi-bin/webscr">' +
+		'<input type="hidden" name="cmd" value="_donations" />'+
+		'<input type="hidden" name="business" value="kayden@globalwheeling.org" />'+
+		'<input type="hidden" name="lc" value="ZA" />'+
+		'<input type="hidden" name="item_name" value="Global Wheeling NGO" />'+
+		'<input type="hidden" name="amount" value="'+ amount + '" />'+
+		'<input type="hidden" name="currency_code" value="GBP" />'+
+		'<input type="hidden" name="no_note" value="0" />'+
+		'<input type="submit" value="Donate to offset CO2" vib="vib" style=" background: -moz-linear-gradient(center top , #A9F5A9, #007A00) repeat scroll 0 0 transparent;" class="vib bl" />'+
+		'</form></div>');
 }
+
 
 function getOffsetCarbonFootprint(position) {
 	var params = {id: itemId, lat: position.coords.latitude, lon: position.coords.longitude};
 	unsafeWindow.console.log("calling backend with data: " + params);
 
 	$.get("http://ebayhackcarboncred.appspot.com/calc", params, processResponse);
-	//processResponse('0.15');
-
-	//alert("Item id: " + itemId + " latitud: " + position.coords.latitude + " longitud: " + position.coords.longitude);
 }
 
 function findItemId(url) {
